fe97324fd28002017d3f5bfd27bd1c97f5c5e1c0
Ricardo Herrera <ricardo.herrera.87@hotmail.com>
2023-01-24 19:35:47 -0600

    Initial commit

---
+++ Esteganografia/Controller/SteganographyLogic.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.ComponentModel;
+
+namespace Steganography.Controller
+{
+   public class SteganographyLogic
+    {
+
+        public int Capacity(byte[] bytes)
+        {
+            int Size = 0;
+            Size = bytes.Length - bytes[10];
+            return Size / 8;
+        }
+
+        public bool CanBeHidden(byte[] bytes, string Message)
+        {
+            bool CanBeHidden_ = false;
+            if (Message.Length > Capacity(bytes))
+                CanBeHidden_ = false;
+            else
+                CanBeHidden_ = true;
+            return CanBeHidden_;
+        }
+
+        public string RevealText(string source, bool NoShowGarbage )
+        {
+            string Message = "";
+            /* JPG
+            * https://docs.fileformat.com/image/jpeg/
+            * 
+            * BMP
+            * https://en.wikipedia.org/wiki/BMP_file_format
+            */
+            byte[] bytes = File.ReadAllBytes(source); // JPG starts from byte 255(0xFF) and BMP from 10 (0xA)
+            int BytePointer = source.Contains(".bmp")?bytes[10]: bytes[255];
+
+            while (BytePointer < bytes.Length - 2)
+            {
+                int WhileCounter = 0;
+                string ByteWord = "";
+                while (WhileCounter < 8)
+                {
+
+                    string currentByte = Convert.ToString(bytes[BytePointer], 2).PadLeft(8, '0');
+                    ByteWord += currentByte[7];
+                    BytePointer += 1;
+                    WhileCounter += 1;
+                }
+
+                if (NoShowGarbage)
+                {
+                    if (Convert.ToChar(Convert.ToInt32(ByteWord, 2)) == '~')
+                    {
+                     
+                        return Message;
+                    }
+
+                }
+
+                Message += Convert.ToChar(Convert.ToInt32(ByteWord, 2));
+
+            }
+
+            return Message;
+        }
+
+        public bool HideText(string source, string dest, string message )
+        {
+
+
+            byte[] bytes = File.ReadAllBytes(source);
+
+            if (!CanBeHidden(bytes, message))
+            {
+                throw new Exception("Text is too large");
+            }
+            /* JPG
+             * https://docs.fileformat.com/image/jpeg/
+             * 
+             * BMP
+             * https://en.wikipedia.org/wiki/BMP_file_format
+             */
+            int BytePointer = source.Contains(".bmp") ? bytes[10] : bytes[255];// JPG starts from byte 255(0xFF) and BMP from 10 (0xA)
+            string CurrentChar = "";
+            int WhileCounter = 0;
+            for (int j = 0; j < message.Length; j++)
+            {
+                int AsciiValue = message[j];
+                CurrentChar = Convert.ToString(AsciiValue, 2).PadLeft(8, '0');
+
+                WhileCounter = 0;
+                while (WhileCounter < 8)
+                {
+                    string currentByte = Convert.ToString(bytes[BytePointer], 2).PadLeft(8, '0');
+                    string newByte = (currentByte.Substring(0, 7) + CurrentChar[WhileCounter]).PadLeft(8, '0');
+                    bytes[BytePointer] = (byte)Convert.ToInt32(newByte, 2);
+                    BytePointer += 1;
+                    WhileCounter += 1;
+                }              
+            }
+
+            CurrentChar = Convert.ToString('~', 2).PadLeft(8, '0');
+            WhileCounter = 0;
+            while (WhileCounter < 8)
+            {
+                string currentByte = Convert.ToString(bytes[BytePointer], 2).PadLeft(8, '0');
+                string newByte = (currentByte.Substring(0, 7) + CurrentChar[WhileCounter]).PadLeft(8, '0');
+                bytes[BytePointer] = (byte)Convert.ToInt32(newByte, 2);
+                BytePointer += 1;
+                WhileCounter += 1;
+            }
+
+            System.IO.FileStream _FileStream = new System.IO.FileStream(dest, System.IO.FileMode.Create, System.IO.FileAccess.Write);
+            _FileStream.Write(bytes, 0, bytes.Length);
+            _FileStream.Close();
+            return true;
+        }
+    }
+}
---
+++ Esteganografia/MainWindow.xaml.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Steganography.Controller;
+using System.ComponentModel;
+using System.Collections;
+
+namespace Steganography
+{
+    /// <summary>
+    /// Logic MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+
+        private readonly BackgroundWorker worker_Reveal = new BackgroundWorker();
+
+        public MainWindow()
+        {
+            worker.DoWork += worker_DoWork;
+            worker.RunWorkerCompleted += worker_RunWorkerCompleted;
+            //worker.ProgressChanged += new ProgressChangedEventHandler(worker_ProgressChanged);
+            //worker.WorkerReportsProgress = true;
+
+            //worker_Reveal.WorkerReportsProgress = true;
+            worker_Reveal.DoWork += workerReveal_DoWork;
+            worker_Reveal.RunWorkerCompleted += workerReveal_RunWorkerCompleted;
+            //worker_Reveal.ProgressChanged += new ProgressChangedEventHandler(workerReveal_ProgressChanged);
+
+            InitializeComponent();
+        }
+
+        private void worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Hashtable parameters =(Hashtable) e.Argument;
+            SteganographyLogic Logic = new SteganographyLogic();
+            Logic.HideText(parameters["textBoxSrc"].ToString(), parameters["textBoxDest"].ToString(), parameters["textBoxMessage"].ToString());
+
+        }
+
+        private void worker_RunWorkerCompleted(object sender,
+                                               RunWorkerCompletedEventArgs e)
+        {
+            labelStatus.Content = "";
+            button1.IsEnabled = true;
+            button2.IsEnabled = true;
+        }
+
+        private void workerReveal_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Hashtable parameters = (Hashtable)e.Argument;
+            SteganographyLogic Logic = new SteganographyLogic();
+            e.Result= Logic.RevealText(parameters["textBoxDest"].ToString(), (bool)parameters["checkBoxGarbageAvoid"]);
+        }
+
+        private void workerReveal_RunWorkerCompleted(object sender,
+                                               RunWorkerCompletedEventArgs e)
+        {
+            labelStatus.Content = "";
+            textBoxShowMessage.Text = e.Result.ToString();
+            button1.IsEnabled = true;
+            button2.IsEnabled = true;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            Hashtable parameters = new Hashtable();
+            try
+            {
+
+                parameters.Add("textBoxSrc", textBoxSrc.Text);
+                parameters.Add("textBoxDest", textBoxDest.Text);
+                parameters.Add("textBoxMessage", textBoxMessage.Text);
+                worker.RunWorkerAsync(parameters);
+                labelStatus.Content = "Hiding the message, please wait...";
+                button1.IsEnabled = false;
+                button2.IsEnabled = false;
+            }
+            catch (Exception ex) 
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+           Hashtable parameters = new Hashtable();
+ 
+           try
+           {
+
+               parameters.Add("textBoxDest", textBoxDest.Text);
+               parameters.Add("checkBoxGarbageAvoid", (bool)checkBoxGarbageAvoid.IsChecked);
+               worker_Reveal.RunWorkerAsync(parameters);
+               labelStatus.Content = "Revealing the message, please wait...";
+               button1.IsEnabled = false;
+               button2.IsEnabled = false;
+               BitmapImage myBitmapImage = new BitmapImage();
+               myBitmapImage.BeginInit();
+               myBitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+               myBitmapImage.UriSource = new Uri(textBoxDest.Text);
+               myBitmapImage.EndInit();
+               image2.Source = myBitmapImage;
+           }
+           catch (Exception ex)
+           {
+               MessageBox.Show(ex.Message);
+           }
+        }
+
+        private void buttonBrowseSource_Click(object sender, RoutedEventArgs e)
+        {
+            SteganographyLogic Logic = new SteganographyLogic();
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".bmp";
+            dlg.Filter = "Bitmap Images (.bmp)|*.bmp|JPG (.JPG)|*.jpg" ;
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                // Open document
+                string filename = dlg.FileName;
+                textBoxSrc.Text = filename;
+                textBoxDest.Text = filename.Contains(".bmp")? filename.Replace(".bmp", "_output.bmp"): filename.Replace(".jpg", "_output.jpg");
+                BitmapImage myBitmapImage = new BitmapImage();
+                myBitmapImage.BeginInit();
+                myBitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                myBitmapImage.UriSource = new Uri(filename);
+                myBitmapImage.EndInit();
+                image1.Source = myBitmapImage;
+                labelLimitWords.Content = Logic.Capacity(File.ReadAllBytes(filename));
+            }
+        }
+
+        private void buttonBrowseDest_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.DefaultExt = ".bmp";
+            dlg.Filter = "Bitmap Images (.bmp)|*.bmp|JPG (.JPG)|*.jpg";
+            Nullable<bool> result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                string filename = dlg.FileName;
+                textBoxDest.Text = filename;
+                BitmapImage myBitmapImage = new BitmapImage();
+                myBitmapImage.BeginInit();
+                myBitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                myBitmapImage.UriSource = new Uri(filename);
+                myBitmapImage.EndInit();
+                image2.Source = myBitmapImage;
+                
+            }
+        }
+
+        private void textBoxMessage_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            labelCurrentWords.Content = textBoxMessage.Text.Length;
+        }
+
+        private void textBoxShowMessage_PreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            labelWordsShowed.Content = textBoxShowMessage.Text.Length;
+        }
+
+        private void textBoxShowMessage_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            labelWordsShowed.Content = textBoxShowMessage.Text.Length;
+        }
+    }
+}
